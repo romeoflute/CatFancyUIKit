d6d7f52e54a751321be71eb27c6fb27b49505620
Romeo Flauta <meoflauta@gmail.com>
2024-03-14 13:30:33 +0800

    Add networking layer for fetching breeds

---
+++ CatFancyUIKit/Helpers/BreedsLoader.swift
@@ -0,0 +1,23 @@
+//
+//  BreedsLoader.swift
+//  CatFancyUIKit
+//
+//  Created by Romeo Flauta on 3/14/24.
+//
+
+import Foundation
+
+enum BreedsLoader {
+    static func loadBreeds() async throws -> [Breed] {
+        let urlString = "https://assets.algoexpert.io/course-assets/iosexpert/breeds.json"
+        guard let url = URL(string: urlString) else {
+            fatalError(URL.couldNotInit(urlString: urlString))
+        }
+        
+        let (data, _) = try await URLSession.shared.data(from: url)
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        let breeds = try decoder.decode(Breeds.self, from: data)
+        return breeds.breeds
+    }
+}
