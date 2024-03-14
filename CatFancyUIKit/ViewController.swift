9a41a96f8817c72361dafef5fecb428f4857577a
Romeo Flauta <meoflauta@gmail.com>
2024-03-14 13:35:05 +0800

    Fetch breeds from viewcontroller

--- CatFancyUIKit/ViewController.swift
+++ CatFancyUIKit/ViewController.swift
@@ -8,12 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        Task {
+            do {
+                let breeds = try await BreedsLoader.loadBreeds()
+                print("BreedsLoader retrieved \(breeds.count) breed(s).")
+            } catch {
+                print("An error happened during breed loading.")
+            }
+        }
     }
-
-
+    
+    
 }
 
