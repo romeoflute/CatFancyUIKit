a3c820e2c29de00bdefa51df84daf5a0ea0ed3e6
Romeo Flauta <meoflauta@gmail.com>
2024-03-14 17:23:18 +0800

    Use the url abstraction

--- CatFancyUIKit/Helpers/BreedsLoader.swift
+++ CatFancyUIKit/Helpers/BreedsLoader.swift
@@ -9,12 +9,7 @@ import Foundation
 
 enum BreedsLoader {
     static func loadBreeds() async throws -> [Breed] {
-        let urlString = "https://assets.algoexpert.io/course-assets/iosexpert/breeds.json"
-        guard let url = URL(string: urlString) else {
-            fatalError(URL.couldNotInit(urlString: urlString))
-        }
-        
-        let (data, _) = try await Current.settings.sessionType.session.data(from: url)
+        let (data, _) = try await Current.settings.sessionType.session.data(from: Current.settings.breedsURL.url)
         let decoder = JSONDecoder()
         decoder.keyDecodingStrategy = .convertFromSnakeCase
         let breeds = try decoder.decode(Breeds.self, from: data)
