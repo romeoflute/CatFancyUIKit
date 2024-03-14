149d6bb0e10e36f452485e3d304f9e9171c4335f
Romeo Flauta <meoflauta@gmail.com>
2024-03-14 16:14:07 +0800

    Use SettingsView in SettingsVC to enable sorting by name or by popularity

--- CatFancyUIKit/Controllers/SettingsVC.swift
+++ CatFancyUIKit/Controllers/SettingsVC.swift
@@ -7,4 +7,46 @@
 
 import UIKit
 
-class SettingsVC: UIViewController {}
+class SettingsVC: UIViewController {
+    var settingsView: SettingsView {
+        if let castedView = view as? SettingsView {
+            return castedView
+        } else {
+            fatalError(fatalCastMessage(view: SettingsView.self))
+        }
+    }
+    
+    override func loadView() {
+        view = SettingsView(frame: UIScreen.main.bounds)
+        title = "Settings"
+        
+        settingsView.sortOrderControl.addTarget(
+            self,
+            action: #selector(sortOrderChanged(_:)),
+            for: .valueChanged
+        )
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateControl()
+    }
+    
+    private func updateControl() {
+        switch SortOrder.current {
+        case .name:
+            settingsView.sortOrderControl.selectedSegmentIndex = 0
+        case .popularity:
+            settingsView.sortOrderControl.selectedSegmentIndex = 1
+        }
+    }
+    
+    @objc func sortOrderChanged(_ sender: UISegmentedControl) {
+        let index = settingsView.sortOrderControl.selectedSegmentIndex
+        if index == 0 {
+            SortOrder.current = .name
+        } else if index == 1 {
+            SortOrder.current = .popularity
+        }
+    }
+}
