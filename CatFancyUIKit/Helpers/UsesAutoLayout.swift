5eb4fdb757915274c3f8adda1175b11ffc634e71
Romeo Flauta <meoflauta@gmail.com>
2024-03-14 14:04:05 +0800

    Add property wrapper to signal a view's use of autolayout

---
+++ CatFancyUIKit/Helpers/UsesAutoLayout.swift
@@ -0,0 +1,22 @@
+//
+//  UsesAutoLayout.swift
+//  CatFancyUIKit
+//
+//  Created by Romeo Flauta on 3/14/24.
+//
+
+import UIKit
+
+@propertyWrapper
+public struct UsesAutoLayout<T: UIView> {
+    public var wrappedValue: T {
+        didSet {
+            wrappedValue.translatesAutoresizingMaskIntoConstraints = false
+        }
+    }
+    
+    public init(wrappedValue: T) {
+        self.wrappedValue = wrappedValue
+        wrappedValue.translatesAutoresizingMaskIntoConstraints = false
+    }
+}
