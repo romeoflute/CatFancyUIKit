d6a774906a51c1d7f8d3734780ddf19c632d9e0f
Romeo Flauta <meoflauta@gmail.com>
2024-03-14 13:51:04 +0800

    Centralize layout measurements

---
+++ CatFancyUIKit/Helpers/Layout.swift
@@ -0,0 +1,14 @@
+//
+//  Layout.swift
+//  CatFancyUIKit
+//
+//  Created by Romeo Flauta on 3/14/24.
+//
+
+import Foundation
+
+struct Layout {
+    static let standard: CGFloat = 8.0
+    static let doubleStandard: CGFloat = 16.0
+    static let defaultHorizontalMargin: CGFloat = 16.0
+}
