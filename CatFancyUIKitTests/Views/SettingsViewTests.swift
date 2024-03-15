b2c3edb6ea1121b6742470fe33d6c0a19273216e
Romeo Flauta <meoflauta@gmail.com>
2024-03-15 14:20:48 +0800

    Add unit test for SettingsView

---
+++ CatFancyUIKitTests/Views/SettingsViewTests.swift
@@ -0,0 +1,16 @@
+//
+//  SettingsViewTests.swift
+//  CatFancyUIKitTests
+//
+//  Created by Romeo Flauta on 3/15/24.
+//
+
+@testable import CatFancyUIKit
+import XCTest
+
+class SettingsViewTests: XCTestCase {
+    func testInitialization() {
+        let sv = SettingsView()
+        XCTAssertNotNil(sv)
+    }
+}
