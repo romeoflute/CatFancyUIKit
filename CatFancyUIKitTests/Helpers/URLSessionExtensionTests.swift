be6bc2aef2c6722781331a000dbcfa9310d07995
Romeo Flauta <meoflauta@gmail.com>
2024-03-15 13:52:13 +0800

    Add unit tests for URLSessionExtension

---
+++ CatFancyUIKitTests/Helpers/URLSessionExtensionTests.swift
@@ -0,0 +1,27 @@
+//
+//  URLSessionExtensionTests.swift
+//  CatFancyUIKitTests
+//
+//  Created by Romeo Flauta on 3/15/24.
+//
+
+@testable import CatFancyUIKit
+import XCTest
+
+class URLSessionExtensionTests: XCTestCase {
+    override class func setUp() {
+        URLSession.didProcessURLs = false
+    }
+    
+    override class func tearDown() {
+        URLSession.didProcessURLs = false
+    }
+    
+    func testStubData() {
+        Current.settings.breedsURL = .withMore
+        let session = URLSession.stub
+        XCTAssert(session.configuration.protocolClasses?[0] is URLProtocolStub.Type)
+        let expectedTestURLCount = MockData.breedNames.count + BreedsURL.allCases.count
+        XCTAssertEqual(expectedTestURLCount, URLSession.urlDataDict.count)
+    }
+}
